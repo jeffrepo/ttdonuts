4f93b8bf84d2f401b1618ab0b39f92df4eea9297
jeffrepo <jeffer.leo93@gmail.com>
2023-04-04 09:17:47 -0600

    validar factura automatica

---
+++ models/account_move.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, Command, _
+
+import logging
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _post(self, soft=True):
+        res = super()._post(soft=True)
+        logging.warning('el res')
+        logging.warning(res)
+        if res:
+            res.action_process_edi_web_services(with_commit=False)
+        return res
