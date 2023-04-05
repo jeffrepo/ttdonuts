bff313fe4e2e88dbab702da8a928b52ddff882d5
jeffrepo <jeffer.leo93@gmail.com>
2023-04-04 23:07:31 -0600

    update valid invoice

--- models/account_move.py
+++ models/account_move.py
@@ -6,6 +6,9 @@ import logging
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    def calcular(self):
+        logging.warning('a jeff')
+
     def _post(self, soft=True):
         res = super()._post(soft=True)
         logging.warning('el res')
@@ -14,10 +17,24 @@ class AccountMove(models.Model):
             res.action_process_edi_web_services(with_commit=False)
         return res
 
-    def post(self):
-        res = super().post()
-        logging.warning('POST TT')
+class PosORder(models.Model):
+    _inherit = "pos.order"
+
+    def _create_invoice(self, move_vals):
+        res = super()._create_invoice(move_vals)
+        logging.warning('create inmvoice')
         logging.warning(res)
-        if res:
-            res.action_process_edi_web_services(with_commit=False)
+        logging.warning(res.state)
+        return res
+
+    def _generate_pos_order_invoice(self):
+        res = super()._generate_pos_order_invoice()
+        logging.warning('_generate_pos_order_invoice')
+        logging.warning(res)
+        move_id = self.env['account.move'].search([('id','=', res['res_id'])])
+        logging.warning('move_id_encontrado')
+        logging.warning(move_id)
+        logging.warning(move_id.state)
+        if move_id:
+            move_id.action_process_edi_web_services(with_commit=False)
         return res
